9f1760ca1e73d8bf25572d9b2f74870cd2e85914
Luciano Uribe <lucianouribe@gmail.com>
2016-11-16 10:20:42 -0700

    Some gem decoration

--- palindrome.rb
+++ palindrome.rb
@@ -1,7 +1,25 @@
 require 'pry'
+require 'colorize'
 
 def start
-  puts "Give a word or frase or quit to exit"
+  puts """
+========================================
+||      THE WEIRD PALINDROME TEASER   ||
+========================================
+
+ _____
+/     \\
+vvvvvvv  /|__/|
+   I   /O,O   |
+   I /_____   |      /|/|
+  J|/^ ^ ^ \\  |    /00  |    _//|
+   |^ ^ ^ ^ |W|   |/^^\\ |   /oo |
+    \\m___m__|_|    \\m_m_|   \\mm_|
+
+
+  """.colorize(:yellow)
+  puts "Give a word or frase or type 'quit' to exit".colorize(:green)
+  print ">> ".colorize(:red)
   @answer = gets.strip
   exit if @answer == 'quit'
   magic
@@ -11,7 +29,7 @@ def magic
   @answer.split('')
   # @answer = @answer.delete(/(\,|\!|\?|\.)|d+/)
   if @answer.length % 2 == 0
-    puts "No palindrome here"
+    puts "No palindrome here!!".colorize(:red)
   else
     reverse
   end
@@ -28,9 +46,9 @@ end
 
 def messages
   if @super_new == @answer
-    puts "It's a palindrome!!!"
+    puts "It's a palindrome!!!".colorize(:blue)
   else
-    puts "no palindrome here"
+    puts "No palindrome here!!".colorize(:red)
   end
 end
 
